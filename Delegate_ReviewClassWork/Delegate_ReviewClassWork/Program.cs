76f6f0a4ec990c3c0ab88eaac30778894aa559a4
msaadat03 <saadatam@code.edu.az>
2022-07-13 00:58:09 +0300

    HomeW16

---
+++ Delegate_ReviewClassWork/Delegate_ReviewClassWork/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate_ReviewClassWork
+{
+    class Program
+    {
+        public delegate bool CheckNums(int num);
+
+        public delegate void ChangeString(string str);
+
+        public delegate int StringLength(string str);
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+            var result = numbers.FindAll(m => m > 7);
+            var result2 = numbers.FindAll(CheckMoreThanFive);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Test(StringToUpper, "Seadet");
+
+            ChangeString change = new ChangeString(StringToUpper);
+            change.Invoke("Seadet");
+
+            Action<string> action = StringToUpper;
+            action += StringToLower;
+            action("Seadet");
+
+            Console.WriteLine(Test(StrLength, "Seadet"));
+
+        }
+
+        public static int Test(StringLength func, string str)
+        {
+            return func(str);
+        }
+
+        public static void StringToUpper(string str)
+        {
+            Console.WriteLine(str.ToUpper());
+        }
+
+        public static void StringToLower(string str)
+        {
+            Console.WriteLine(str.ToLower());
+        }
+
+
+
+        public static int StrLength(string str)
+        {
+            return str.Length;
+        }
+
+       
+
+        public static void Test(ChangeString func, string str)
+        {
+            func(str);
+        }
+
+        public static bool CheckOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool CheckEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool CheckMoreThanFive(int number)
+        {
+            return number > 7;
+        }
+
+
+        public static int Sum(CheckNums func, int[] nums)
+        {
+            var sum = 0;
+            foreach (var item in nums)
+            {
+                if (func(item))
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+
+        public static int Sum(Predicate<int> predicate, int[] nums)
+        {
+            var sum = 0;
+
+            foreach (var item in nums)
+            {
+                if (predicate(item))
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+    }
+  }
---
+++ HomeW16_Delegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeW16_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> name = new List<string>() { "Saadat", "Fidan", "Cavid" };
+            Cheking(CheckName, name);
+        }
+
+        public static bool CheckName(string name)
+        {
+            return name != "Cavid";
+        }
+
+        public static void Cheking(Predicate<string> predicate, List<string> names)
+        {
+            foreach (var item in names)
+            {
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+//Cheking(CheckName, name);
